e68440db30894f6c043b43dba75d1798624cd83e
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-09-05 09:06:57 +0000

    rest

--- app/controllers/public/works_controller.rb
+++ app/controllers/public/works_controller.rb
@@ -1,13 +1,49 @@
 class Public::WorksController < ApplicationController
+  
   def index
+    @works = Work.page(params[:page])
   end
 
   def new
+    @work = Work.new
+  end
+  
+  def create
+    @work = current_user.works.new(work_params)
+    if @work.save
+      redirect_to work_path(@work)
+    else
+      render :new
+    end
   end
 
   def show
+    @work = Work.find(params[:id])
   end
 
   def edit
+    @work = Work.find(params[:id])
   end
+  
+  def update
+    @work = Work.find(params[:id])
+    if @work.update
+      redirect_to work_path(@work)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    work = Work.find(params[:id])
+    work.destoy
+    redirect_to work_path(work)
+  end
+  
+  private
+  
+  def work_params
+    params.require(:work).permit(:title,:caption,:main_image,sub_images: [])
+  end
+  
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,9 @@
 class Work < ApplicationRecord
+  
+  belongs_to :user
+  
+  has_one_attached :main_image
+  has_many_attached :sub_images
+  
+  
 end
